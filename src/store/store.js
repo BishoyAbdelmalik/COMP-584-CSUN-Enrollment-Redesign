a333bdcffc3343da6cc129fb5c545f8633772526
Bishoy Abdelmalik <15853596+BishoyAbdelmalik@users.noreply.github.com>
2022-04-22 15:44:08 -0700

    feat: add classes slice & major to main Subject

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import classesSlice  from '../reducers/classesSlice';
 import profileSlice from '../reducers/profileSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     profile: profileSlice,
+    classes: classesSlice
   },
 });
