d721e5682be6ab04337ed114c79fe9283e58f49f
Bishoy Abdelmalik <15853596+BishoyAbdelmalik@users.noreply.github.com>
2022-04-22 22:30:19 -0700

    refactor: unnecessary imports

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../reducers/profileSlice";
 import LoginForm from "./loginForm";
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { selectStatus } from "../reducers/profileSlice";
 // import style from "./../App.module.scss";
 import { useSelector } from 'react-redux';
