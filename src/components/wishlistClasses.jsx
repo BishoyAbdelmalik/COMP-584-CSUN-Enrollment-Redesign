c4b1a8c315d826d762d0025d4923e763e71e3b7c
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2022-04-27 14:00:29 -0700

    feat: started wishlist component

---
+++ src/components/wishlistClasses.jsx
@@ -0,0 +1,7 @@
+const WishlistClasses = ({classes}) => {
+    return ( <>
+    {classes.length===0?<h2>No wish list</h2>:<p></p>}
+    </> );
+}
+ 
+export default WishlistClasses;
--- src/routes/wishlist.jsx
+++ src/routes/wishlist.jsx
@@ -1,8 +1,13 @@
-export default function Wishlist() {
-    return (
-      <>
-        <h2 className="text-center">My Bookmarked Courses</h2>
+import { useSelector } from "react-redux";
+import WishlistClasses from "../components/wishlistClasses";
+import { selectWishlist } from "../reducers/wishlistSlice";
 
-      </>
-    );
-  }
+export default function Wishlist() {
+  const wishlist = useSelector(selectWishlist);
+  return (
+    <>
+      <h2 className="text-center">My Bookmarked Courses</h2>
+      <WishlistClasses classes={wishlist}/>
+    </>
+  );
+}
