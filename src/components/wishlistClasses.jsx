949571bed3096edf588d8160a8f48096615331f1
Bishoy Abdelmalik <15853596+BishoyAbdelmalik@users.noreply.github.com>
2022-04-27 18:46:10 -0700

    feat: add drop down and style component

--- src/components/wishlistClasses.jsx
+++ src/components/wishlistClasses.jsx
@@ -1,7 +1,12 @@
 import WishlistSection from "./wishlistSection";
 
 const WishlistClasses = ({ classes }) => {
-    const catagories = [...new Set(classes.map((c) => c.semester))];
+    const catagories = [...new Set(classes.map((c) => c.semester))].sort((a, b) => {
+        if (a === null) {
+            return -1;
+        }
+        return 0;
+    });
     const getClassesForCategory = (category) => classes.filter((c) => c.semester === category);
     return (
         <>
--- src/components/wishlistSection.jsx
+++ src/components/wishlistSection.jsx
@@ -2,12 +2,13 @@ import ClassCompact from "./classCompact";
 
 const WishlistSection = ({ title, classes }) => {
     return (
-        <>
-            <h3>{title ? title : "Unorganized"}</h3>
+        <div className="mb-5">
+            <h4 className="font-weight-bold">{title ? title : "Unorganized"}</h4>
+            <hr className="m-0 mb-1"></hr>
             {classes.map((c, index) => (
                 <ClassCompact key={index} id={c.id} name={c.name} units={c.units} />
             ))}
-        </>
+        </div>
     );
 }
 
