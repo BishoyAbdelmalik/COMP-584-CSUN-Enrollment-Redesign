57548c92f6a4b3045725337ab7b578711517b205
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2022-05-04 13:03:53 -0700

    refactor: change fetch location

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,53 +1,12 @@
-import { selectStatus, login, logout, selectMainSubject } from "../reducers/profileSlice";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase";
-
-// import style from "./../App.module.scss";
-
+import { selectStatus } from "../reducers/profileSlice";
+import React from "react";
+import { useSelector } from "react-redux";
 import Login from "../components/login";
-
-import Signup from "../components/Signup";
 import Search from "./search";
-import { addGEClass, addMainSubjectClass } from "../reducers/classesSlice";
-import { getClasses, getGEClasses } from "../api/utils";
-import { signin } from "../actions/auth";
+
 
 const Home = () => {
   const status = useSelector(selectStatus);
-  const dispatch = useDispatch();
-  const mainSubject = useSelector(selectMainSubject);
-  useEffect(() => {
-    if (status === 'logged-in') {
-      getGEClasses().then(response => dispatch(addGEClass({ ge: response })));
-    }
-  }, [status, dispatch]);
-
-  useEffect(() => {
-    if (status === 'logged-in') {
-      getClasses(mainSubject).then(response => dispatch(addMainSubjectClass({ mainSubject: response })));
-    }
-  }, [mainSubject, status, dispatch]);
-
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
-      if (currentuser) {
-        // user is logged in, send the user's details to redux, store the current user in the state
-
-        signin(dispatch,currentuser);
-
-      } else {
-        dispatch(logout());
-      }
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, [dispatch]);
 
   return <>{!status ? <Login /> : <Search />}</>;
 };
