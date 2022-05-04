0167530dbc76c39d737ed33445ee43fc287ee533
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2022-05-04 14:01:56 -0700

    feat: title and description

---
+++ src/components/classDescription.jsx
@@ -0,0 +1,9 @@
+
+export const ClassDescription = ({description}) => {
+    return(
+        <div>
+            <h4>Course Description</h4>
+            <p>{description}</p>
+        </div>
+    )
+}
--- src/routes/class.jsx
+++ src/routes/class.jsx
@@ -1,25 +1,43 @@
+import classNames from "classnames";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { getClasses } from "../api/utils";
-
-export const Class = () =>{
-    const {id}=useParams();
-    const [sections,setSections]=useState([]);
-    const [title,setTitle]=useState("");
-    const [description,setDescription]=useState("");
-    const [number,setNumber]=useState(id.toUpperCase());
-    const [units,setUnits]=useState(0);
-    if(sections.length===0){
-        getClasses(id).then((d)=>(setSections(d)));
+import { MdKeyboardArrowLeft } from "react-icons/md"
+import { ClassTitle } from "../components/classTitle";
+import { Spinner } from "react-bootstrap";
+import { ClassDescription } from "../components/classDescription";
+export const Class = () => {
+    const { id } = useParams();
+    const [sections, setSections] = useState([]);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [number, setNumber] = useState(id.toUpperCase());
+    const [units, setUnits] = useState(0);
+    if (sections.length === 0) {
+        getClasses(id).then((d) => (setSections(d)));
     }
-    useEffect(()=>{
-        if(sections.length>0){
+    useEffect(() => {
+        if (sections.length > 0) {
             setTitle(sections[0].title);
             setNumber(`${sections[0].subject}-${sections[0].catalog_number}`);
             setUnits(sections[0].units);
             setDescription(sections[0].description);
         }
         console.log(sections);
-    },[sections])
-    return (<p>{id}</p>)
+    }, [sections]);
+    return (
+        <>
+            <Link to={"/search"} className={classNames("text-muted", "text-decoration-none", "mb-2")}><MdKeyboardArrowLeft />Back to search</Link>
+            <div>
+                {
+                    title === "" ? <Spinner animation="border" /> :
+                        <>
+                            <ClassTitle id={number} title={title} units={units} />
+                            <ClassDescription description={description}/>
+                        </>
+
+                }
+            </div>
+        </>
+    );
 }
