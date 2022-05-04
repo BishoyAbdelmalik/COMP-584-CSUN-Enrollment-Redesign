222955f193adff98f31cb5eff264b18fe07361c0
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2022-05-04 13:24:40 -0700

    feat: save class data in component

--- src/routes/class.jsx
+++ src/routes/class.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { getClasses } from "../api/utils";
 
 export const Class = () =>{
     const {id}=useParams();
-    getClasses(id).then((data)=>{console.log(data)});
+    const [sections,setSections]=useState([]);
+    const [title,setTitle]=useState("");
+    const [description,setDescription]=useState("");
+    const [number,setNumber]=useState(id.toUpperCase());
+    const [units,setUnits]=useState(0);
+    if(sections.length===0){
+        getClasses(id).then((d)=>(setSections(d)));
+    }
+    useEffect(()=>{
+        if(sections.length>0){
+            setTitle(sections[0].title);
+            setNumber(`${sections[0].subject}-${sections[0].catalog_number}`);
+            setUnits(sections[0].units);
+            setDescription(sections[0].description);
+        }
+        console.log(sections);
+    },[sections])
     return (<p>{id}</p>)
 }
