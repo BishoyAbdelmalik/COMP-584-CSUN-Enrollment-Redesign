9fc14bcef5b767c58254d4d37e8d0e1ad0c4cc72
tarun-vihar <tarunvihar21@gmail.com>
2022-05-12 15:47:32 -0700

    feat: added course page

--- src/reducers/wishlistSlice.js
+++ src/reducers/wishlistSlice.js
@@ -1,41 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [];
 
 export const wishlistSlice = createSlice({
-    name: 'wishlist',
-    initialState,
-    // The `reducers` field lets us define reducers and generate associated actions
-    reducers: {
-      addClass: (state, action) => {
-        // Redux Toolkit allows us to write "mutating" logic in reducers. It
-        // doesn't actually mutate the state because it uses the Immer library,
-        // which detects changes to a "draft state" and produces a brand new
-        // immutable state based off those changes
-        
-        /*
+  name: "wishlist",
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    addClass: (state, action) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+
+      /*
         let exampleClass = {
             semester: null | "semester name ie fall 2022",
             id: "comp-110",
             name: "Introduction to Algorithms and Programming",
             units: 3
         }*/
-        const newClassId=action.payload.id;
-        if(state.find(e=>e.id===newClassId)){
-          return state;
-        }
-        return [...state,action.payload]
-      },
-      removeClass: (state,action) => {
-          const classId = action.payload.toLowerCase();
-          return state.filter(({id})=>id!==classId);
+      const newClassId = action.payload.id;
+      if (state.find((e) => e.id === newClassId)) {
+        return state;
       }
+      return [...state, action.payload];
     },
-  });
+    removeClass: (state, action) => {
+      const classId = action.payload.id;
+      return state.filter(({ id }) => id !== classId);
+    },
 
-  export const { addClass, removeClass } = wishlistSlice.actions;
-  
+    setUserWishList: (state, action) => {
+      return [...action.payload];
+    },
+  },
+});
+
+export const { addClass, removeClass, setUserWishList } = wishlistSlice.actions;
+
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
---
+++ src/routes/course.jsx
@@ -0,0 +1,75 @@
+import classNames from "classnames";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import {
+  selectUser,
+  selectMainSubject,
+  setMainSubject,
+} from "../reducers/profileSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useFirebaseDetails } from "../context/collectionProviders";
+import { addAllMajors, selectAllMajors } from "../reducers/classesSlice";
+
+export default function Course() {
+  const navigate = useNavigate();
+  const uuid = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const majors = useSelector(selectAllMajors);
+  const [majorSelected, setMajor] = useState("comp");
+
+  let { fetchMajors, updateUserMajorDetails } = useFirebaseDetails();
+  useEffect(() => {
+    if (majors.length === 0) {
+      fetchMajors()
+        .then((snapshot) => {
+          const allMajors = Object.values(snapshot.val());
+          dispatch(addAllMajors({ allMajors }));
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, []);
+
+  function handleSubmit() {
+    if (majorSelected !== "") {
+      updateUserMajorDetails({ id: majorSelected }, uuid);
+      console.log(majorSelected);
+      dispatch(setMainSubject({ major: majorSelected }));
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <Row>
+          <Col md={4}>Major:</Col>
+          <Col xs="auto" className="my-1">
+            <Form.Select
+              as="select"
+              value={majorSelected}
+              onChange={(e) => setMajor(e.target.value)}
+            >
+              {majors.map((x, i) => (
+                <option key={i + 1} value={x.toUpperCase()}>
+                  {x.toUpperCase()}
+                </option>
+              ))}
+            </Form.Select>
+          </Col>
+        </Row>
+
+        <Button
+          variant="primary"
+          className="rounded-0 mb-1 mt-1 w-100"
+          onClick={() => {
+            handleSubmit();
+          }}
+        >
+          {majorSelected === "" ? "Skip" : "Save Details"}
+        </Button>
+      </div>
+    </>
+  );
+}
