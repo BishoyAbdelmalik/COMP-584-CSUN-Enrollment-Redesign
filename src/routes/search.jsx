4fd215bac06b7894240b616c2a8faaac2ba663e1
mattestes <54647681+mattestes@users.noreply.github.com>
2022-05-12 23:45:05 -0700

    feat: search results displayed

--- src/routes/search.jsx
+++ src/routes/search.jsx
@@ -4,41 +4,85 @@ import appStyles from "./../App.module.scss"
 import classNames from "classnames";
 import GEList from "./../components/geList";
 import SubjSnippet from "../components/subjSnippet";
-import { selectGE, selectMainSubjectClasses } from "../reducers/classesSlice";
+import { selectAllCourses, selectGE, selectMainSubjectClasses } from "../reducers/classesSlice";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 
 export default function Search() {
   const ges = useSelector(selectGE);
   const subj = useSelector(selectMainSubjectClasses);
+  const allCourses = useSelector(selectAllCourses);
+  let [matchingCourses, setMatchingCourses] = useState(allCourses);
   const [selectedType, setSelectedType] = useState("");
   return (
     <>
       <h2 className="text-center">Class Search</h2>
-      <div className={classNames("mx-auto",appStyles.form)}>
-        <SearchBox onSearch={(value)=>{
+      <div className={classNames("mx-auto", appStyles.form)}>
+        <SearchBox onSearch={(value) => {
           console.log(value);
-          if(selectedType!=="search"){
+          if (selectedType !== "search") {
             setSelectedType("search");
           }
+          // Breaks words in search box apart into an array, assuming they are separated by a space or dash.
+          let searchTerms = value.split(/[ -]/);
+          console.log(searchTerms);
+          setMatchingCourses(function () {
+            let subjectMatches = allCourses.filter(function (course) {
+              for (let term of searchTerms) {
+                if (!(course.subject.toLowerCase().startsWith(term.toLowerCase()) ||
+                  course.catalog_number.toLowerCase().startsWith(term.toLowerCase()))) {
+                  return false;
+                }
+              }
+              return true;
+            });
+            // If there weren't any matches from subject and catalog_number, 
+            // then we return results with matches from title as well.
+            if (subjectMatches.length === 0) {
+              let titleMatches = allCourses.filter(function (course) {
+                // Words contains the subject, catalog_number, and words from the title. 
+                // We'll search thru those.
+                let words = [course.subject.toLowerCase(), course.catalog_number.toLowerCase()];
+                words = words.concat(course.title.toLowerCase().split(" "));
+                for (let term of searchTerms) {
+                  let foundMatch = false;
+                  for (let word of words) {
+                    if (word.startsWith(term)) {
+                      foundMatch = true;
+                      break;
+                    }
+                  }
+                  if (!foundMatch) {
+                    return false;
+                  }
+                }
+                return true;
+              });
+              return titleMatches;
+            } else {
+              return subjectMatches;
+            }
+          }
+          );
         }} />
         <Button
           variant="primary"
           className="rounded-0 mb-1 mt-1 w-100"
-          onClick={()=>{setSelectedType("major")}}
+          onClick={() => { setSelectedType("major") }}
         >
           Quick search by main subject
         </Button>
         <Button
           variant="primary"
           className="rounded-0 mb-1 mt-1 w-100"
-          onClick={()=>{setSelectedType("ge")}}
+          onClick={() => { setSelectedType("ge") }}
         >
           Quick search by GE
         </Button>
         <div className="mb-3 mt-3">
-        {selectedType==="ge"&&<GEList ges={ges}/>}
-        {selectedType==="major"&&<SubjSnippet subj={subj}/>}
+          {selectedType === "ge" && <GEList ges={ges} />}
+          {selectedType === "major" && <SubjSnippet subj={subj} />}
+          {selectedType === "search" && <SubjSnippet subj={matchingCourses} />}
         </div>
       </div>
     </>
