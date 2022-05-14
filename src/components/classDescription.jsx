f14b613f47abcc785461289f3084544d82c6f822
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2022-05-13 21:26:30 -0700

    fix: fix html semantics on class description

--- src/components/classDescription.jsx
+++ src/components/classDescription.jsx
@@ -2,20 +2,10 @@ import classNames from "classnames";
 import style from "./classPage.module.scss";
 
 export const ClassDescription = ({ description }) => {
-<<<<<<< HEAD
-    return (
-        <div className={classNames(style.description)}>
-            <h2>Course Description</h2>
-            <p>{description}</p>
-        </div>
-    )
-}
-=======
   return (
     <div className={classNames(style.description)}>
-      <h4>Course Description</h4>
+      <h2>Course Description</h2>
       <p>{description}</p>
     </div>
-  );
-};
->>>>>>> main
+  )
+}
