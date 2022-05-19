e1eeca4206c0c588ec4eb1caa1879215064a507c
Bishoy Abdelmalik <15853596+BishoyAbdelmalik@users.noreply.github.com>
2022-05-18 18:01:29 -0700

    refactor: remove unused stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React, { useEffect } from "react";
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
-// import style from './App.module.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Search from "./routes/search";
 import Wishlist from "./routes/wishlist";
